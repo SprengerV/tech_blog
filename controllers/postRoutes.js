01f2a64d05c8a5b1320ded58dcedc16ce964f62b
SprengerV <b.francis@sprengerv.com>
2021-04-05 11:53:56 -0400

    added function to find single post by id

--- controllers/postRoutes.js
+++ controllers/postRoutes.js
@@ -1,12 +1,37 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth.js');
+const { User, Post, Comment } = require('../models');
 
 // routes for /post/
 router.get('/new', withAuth, (req, res) => {
 
 });
 router.get('/:id', withAuth, (req, res) => {
-
+    Post
+        .findByPk(req.params.id,
+            {
+                include: [
+                    {
+                        model: User,
+                        attributes: ['userName']
+                    },
+                    {
+                        model: Comment,
+                        attributes: ['body', 'createdAt'],
+                        include: [
+                            {
+                                model: User,
+                                attributes: ['userName']
+                            }
+                        ]
+                    }
+                ]
+            }
+        )
+        .then((postData) => {
+            const post = postData.get({ plain: true });
+            res.send(post);
+        })
 });
 
 module.exports = router;
