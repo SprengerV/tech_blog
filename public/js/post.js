b2952a313b259598e1c445aa18678c86007d86bb
SprengerV <b.francis@sprengerv.com>
2021-04-06 22:35:14 -0400

    added in function to make api call for post deletion

--- public/js/post.js
+++ public/js/post.js
@@ -12,7 +12,17 @@ const makePost = async (event) => {
     });
     res.ok ? document.location.replace('/') : alert('Failed to create post');
 }
+const delPost = async (event) => {
+    const postId = $(this).attr('data-postId');
+    const res = await fetch(`/api/post/del/${postId}`, {
+        method: 'DELETE',
+        body: { postId },
+        headers: { 'Content-Type': 'application/json' }
+    })
+    res.ok ? document.location.replace('/') : alert('Failed to delete post');
+}
 
 $(document).ready(() => {
     $('#addPost').submit(makePost);
+    $('.delBtn').click(delPost);
 })
