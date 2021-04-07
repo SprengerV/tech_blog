31b7028044e1b12bb7159e04a97baafc4ed0cd8e
SprengerV <b.francis@sprengerv.com>
2021-04-06 23:46:44 -0400

    made it so user is logged in when signing up

--- controllers/api/authRoutes.js
+++ controllers/api/authRoutes.js
@@ -4,12 +4,23 @@ const User = require('../../models').User;
 router.post('/', (req, res) => {
     User
         .create(req.body)
-        .then((userData) => {
+        .then(userData => {
+            if (!userData) {
+                res
+                    .status(400)
+                    .json({ message: 'User failed to create' });
+                return
+            }
             console.log(`User "${req.body.userName}" registered`);
-            res
-                .status(200)
-                .json(userData);
-        });
+            req.session.save(() => {
+                req.session.userId = userData.id;
+                req.session.loggedIn = true
+                res
+                    .status(200)
+                    .json({ user: userData, message: 'You are now logged in'});
+            });
+        })
+        .catch(err => res.status(500).json(err));
 });
 router.post('/login', (req, res) => {    
     User
@@ -39,14 +50,14 @@ router.post('/login', (req, res) => {
             res
                 .status(500)
                 .json(err)
-        })
-})
+        });
+});
 router.post('/logout', (req, res) => {
     req.session.save(() => {
         req.session.userId = false,
         req.session. loggedIn = false
         res.json({ message: "You are now logged out" })
-    })
-})
+    });
+});
 
 module.exports = router;
