5f25e656f23d4ab02e0c00fcdf9785ea4eefe0be
SprengerV <b.francis@sprengerv.com>
2021-04-09 03:16:29 -0400

    reformatted login page and js to match

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,30 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-
-    if (username && password) {
-        const response = await fetch('/api/auth/login', {
-            method: 'POST',
-            body: JSON.stringify({ 
-                userName: username,
-                password: password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert('Failed to log in');
-        }
+    const userName = $('#loginUsername').val();
+    const password = $('#loginPassword').val();
+    if (!userName) {
+        alert('Please enter username');
+        return
+    }
+    if (!password) {
+        alert('Please enter password');
+        return
+    }
+    const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        body: JSON.stringify({ 
+            userName,
+            password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to log in');
     }
 };
 
 document
-    .querySelector('#login-form')
+    .querySelector('#loginForm')
     .addEventListener('submit', loginFormHandler);
